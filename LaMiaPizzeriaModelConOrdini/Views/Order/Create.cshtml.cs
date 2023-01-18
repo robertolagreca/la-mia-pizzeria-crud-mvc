96c201dc2eef50effa72232ded8fe5c4a3a52953
robertolagreca <robertolagreca92@gmail.com>
2023-01-18 17:25:23 +0100

    File Create.cshtml per gli ordini

--- LaMiaPizzeriaModelConOrdini/Views/Order/Create.cshtml.cs
+++ LaMiaPizzeriaModelConOrdini/Views/Order/Create.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace LaMiaPizzeriaModel.Views.Pizza
+namespace LaMiaPizzeriaModelConOrdini.Views.Order
 {
     public class CreateModel : PageModel
     {
